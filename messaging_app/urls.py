712c62f29b193454d6aede3e445c7dce7fd47f22
Nagulesh N <nagulesh.22cs@kct.ac.in>
2024-03-26 23:31:25 +0530

    unwanted code removed

--- messaging_app/urls.py
+++ messaging_app/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path('login/', views.login,name="login"),
     path('log/', views.Log_view,name="log"),
     path('logout/', auth_views.LogoutView.as_view() ,name="logout"),
-    # path('create_instance/', views.create_instance,name="create_instance"),
     path('delete_all_instance/', views.delete_all_messages,name="delete_all_messages"),
     path('delete_instance/<int:instance_id>', views.delete_instance,name="delete_instance"),
     path('generate_qr/<int:instance_id>',views.generate_qr,name='generate_qr'),
